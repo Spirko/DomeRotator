ed34b7cd051157db4644eac108bf68bb55a96c42
Jeffery Spirko <spirko@gmail.com>
2024-09-16 23:04:01 -0500

    Reorganized frames.

--- DomeRotator.py
+++ DomeRotator.py
@@ -5,32 +5,35 @@ from CameraSelector import CameraSelector
 from CameraDisplay2 import CameraDisplay
 from DomeCommands import DomeCommands
 
+# Requires conda packages:
+#  python=3.12, pyserial, opencv, pygame
+
 class DomeRotator(tk.Tk):
   def __init__(self, **kwargs):
     super().__init__(**kwargs)
     self.title('Dome Rotator')
     self.geometry('800x600')
     
-    self.make_config_frame()
+    self.make_bluetooth_frame()
     self.make_camera_frame()
     self.make_command_frame()
+  
+  def make_bluetooth_frame(self):
+    self.blue_frm = ttk.Frame(master=self, borderwidth=2, relief='raised')
+    self.blue_frm.pack(side=tk.TOP)
     
-  def make_config_frame(self):
-    self.cfg_frm = ttk.Frame(master=self, borderwidth=2, relief='sunken')
-    self.cfg_frm.pack(expand=True)
-    
-    self.com = ComPortSelector(self.cfg_frm)
-    self.com.pack(side=tk.LEFT, fill=tk.X, padx=20)
-    
-    self.cam = CameraSelector(self.cfg_frm)
-    self.cam.pack(side=tk.LEFT, padx=20)
-    
+    blue_lbl = ttk.Label(self.blue_frm, text='Connect Bluetooth to "ESP32 MDrive"')
+    blue_lbl.pack(side=tk.TOP)
+  
   def make_camera_frame(self):
-    self.cam_frm = ttk.Frame(master=self)
+    self.cam_frm = ttk.Frame(master=self, borderwidth=2, relief='sunken')
     self.cam_frm.pack(expand=True)
     
+    self.cam = CameraSelector(self.cam_frm)
+    self.cam.pack(side=tk.TOP)
+    
     self.disp = CameraDisplay(master=self.cam_frm)
-    self.disp.pack(fill=tk.BOTH, expand=True)
+    self.disp.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
     
     self.cam.connect = lambda : \
       self.disp.connect(int(self.cam.combobox.current()))
@@ -38,16 +41,19 @@ class DomeRotator(tk.Tk):
       self.disp.disconnect()
     
   def make_command_frame(self):
-    self.cmd_frm = ttk.Frame(master=self)
-    self.cmd_frm.pack(expand=True)
+    self.cmd_frm = ttk.Frame(master=self, borderwidth=2, relief='sunken')
+    self.cmd_frm.pack()
 
+    self.com = ComPortSelector(self.cmd_frm)
+    self.com.pack(side=tk.LEFT, fill=tk.X, padx=20)
+    
     self.dome = DomeCommands(self.cmd_frm)
     self.dome.pack()
   
     self.com.combobox.bind("<<ComboboxSelected>>", 
         lambda arg: self.dome.open(self.com.combobox.get()))
 
-        
+
 if __name__ == "__main__":
   app = DomeRotator()
   app.mainloop()
